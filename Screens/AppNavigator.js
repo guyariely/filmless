930b8817313d8b9b5e2b697a42e731819bfe4a31
Guy Arieli <guyarieli1997@gmail.com>
2019-08-24 12:18:54 +0300

    removed the material bottom navigator in favor of createMaterialTopTabNavigator

--- Screens/AppNavigator.js
+++ Screens/AppNavigator.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { createAppContainer } from "react-navigation";
-import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+import { createMaterialTopTabNavigator, createAppContainer } from "react-navigation";
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import DiscoverScreen from './DiscoverScreen/DiscoverScreen';
 import WatchListScreen from './WatchListScreen';  
 import SettingsScreen from './SettingsScreen';
-import Colors from '../Constants/colors';
+import colors from '../Constants/colors';
 
-const AppNavigator = createMaterialBottomTabNavigator(
+const AppNavigator = createMaterialTopTabNavigator(
   {
     Discover: { 
       screen: DiscoverScreen,
@@ -40,14 +39,68 @@ const AppNavigator = createMaterialBottomTabNavigator(
   }, 
   {
     initialRouteName: 'Discover',
-    labeled: false,
-    activeColor: Colors.text01,
-    inactiveColor: Colors.text02,
-    barStyle: { 
-      backgroundColor: Colors.base01,
-      shadowOpacity: 0
-    },
+    tabBarPosition: 'bottom',
+    swipeEnabled: false,
+    animationEnabled: false,
+    tabBarOptions: {
+      activeTintColor: colors.text01,
+      inactiveTintColor: colors.text02,
+      style: { 
+        backgroundColor: colors.base01,
+        paddingBottom: 30
+      },
+      indicatorStyle: {
+        height: 0
+      },
+      showIcon: true,
+      showLabel: false
+    }
   }
 );
 
 export default createAppContainer(AppNavigator);
+
+// const AppNavigator = createMaterialBottomTabNavigator(
+  //   {
+  //     Discover: { 
+  //       screen: DiscoverScreen,
+  //       navigationOptions: {
+  //         tabBarLabel: "",
+  //         tabBarIcon: ({tintColor}) => (
+  //           <Icon name="local-movies" color={tintColor} size={28} />
+  //         )
+  //       } 
+  //     },
+  //     WatchList: { 
+  //       screen: WatchListScreen,
+  //       navigationOptions: {
+  //         tabBarLabel: "",
+  //         tabBarIcon: ({tintColor}) => (
+  //           <Icon name="bookmark" color={tintColor} size={28} />
+  //         )
+  //       } 
+  //     },
+  //     Settings: { 
+  //       screen: SettingsScreen,
+  //       navigationOptions: {
+  //         tabBarLabel: "",
+  //         tabBarIcon: ({tintColor}) => (
+  //           <Icon name="settings" color={tintColor} size={28} />
+  //         )
+  //       } 
+  //     }
+  //   }, 
+  //   {
+  //     initialRouteName: 'Discover',
+  //     labeled: false,
+  //     activeColor: Colors.text01,
+  //     inactiveColor: Colors.text02,
+  //     barStyle: { 
+  //       backgroundColor: Colors.base01,
+  //       shadowOpacity: 0
+  //     },
+  //   }
+  // );
+  
+  // export default createAppContainer(AppNavigator);
+  
