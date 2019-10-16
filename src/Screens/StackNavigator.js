53e59f4603c2ba049c954a6bec0a3319c106e091
Guy Arieli <guyarieli1997@gmail.com>
2019-10-16 15:38:10 +0300

    bug fix in the navigation stack

--- src/Screens/StackNavigator.js
+++ src/Screens/StackNavigator.js
@@ -1,23 +1,31 @@
-import TabsNavigator from './Tabs/TabsNavigator';
-import MovieScreen from './MovieScreen/MovieScreen';
-import Lightbox from './Lightbox';
-import ReviewScreen from './ReviewScreen/ReviewScreen';
-import PersonScreen from './PersonScreen/PersonScreen';
-import BiographyScreen from './BiographyScreen/BiographyScreen';
-import HidelistScreen from './HidelistScreen/HidelistScreen';
-import { createStackNavigator } from 'react-navigation-stack';
+import TabsNavigator from "./Tabs/TabsNavigator";
+import MovieScreen from "./MovieScreen/MovieScreen";
+import Lightbox from "./Lightbox";
+import ReviewScreen from "./ReviewScreen/ReviewScreen";
+import PersonScreen from "./PersonScreen/PersonScreen";
+import BiographyScreen from "./BiographyScreen/BiographyScreen";
+import HidelistScreen from "./HidelistScreen/HidelistScreen";
+import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 
-const StackNavigator = createStackNavigator({
-  Tabs: TabsNavigator,
-  MovieScreen: MovieScreen,
-  Lightbox: Lightbox,
-  ReviewScreen: ReviewScreen,
-  PersonScreen: PersonScreen,
-  BiographyScreen: BiographyScreen,
-  HidelistScreen: HidelistScreen
-}, {
-  headerMode: 'none'
-});
+const StackNavigator = createStackNavigator(
+  {
+    Tabs: {
+      screen: TabsNavigator,
+      navigationOptions: {
+        gesturesEnabled: false
+      }
+    },
+    MovieScreen: MovieScreen,
+    Lightbox: Lightbox,
+    ReviewScreen: ReviewScreen,
+    PersonScreen: PersonScreen,
+    BiographyScreen: BiographyScreen,
+    HidelistScreen: HidelistScreen
+  },
+  {
+    headerMode: "none"
+  }
+);
 
 export default createAppContainer(StackNavigator);
